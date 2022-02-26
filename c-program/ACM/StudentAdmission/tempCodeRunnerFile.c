03c9835380887f1392191a8d3e7cdd6138df033c
rohit27-2 <gaddamrohitgoud@gmail.com>
2022-02-26 17:32:20 +0530

    First Commit

---
+++ c-program/ACM/StudentAdmission/tempCodeRunnerFile.c
@@ -0,0 +1,14 @@
+char arr[11][30]= {"1 BVRIT","2 CMR",
+                "3 Gokaraju Rangaraju ","4 IIIT ","5 MLR ",
+                "6 MGIT",
+                "7 Malla Reddy College",
+                "8 Muffakham Jah College",
+                "9 VNR Vignana Jyothi",
+                "10 Anurag University",
+                "11 KMIT"};
+
+
+for(int i= 1; i<=11; i++)
+{
+    printf("%s", arr[i]);
+}
